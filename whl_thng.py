0485518b7f500bda973073ad588ccf10a955ec67
dudeSelvin <104858268+dudeSelvin@users.noreply.github.com>
2022-05-19 00:51:04 +0530

    Add files via upload

---
+++ whl_thng.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+from twilio.rest import Client 
+ 
+account_sid = 'ACaaa01536cde6fb1770205905031765ad' 
+auth_token = 'cecbed9876e8bf12489a07358dae73b4' 
+client = Client(account_sid, auth_token) 
+#GPIO SETUP
+channel = 8
+channel2 = 7
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+GPIO.setup(channel2, GPIO.IN)
+ 
+def callback(channel):
+    print("flame detected")
+    message = client.messages.create( 
+                              from_='whatsapp:+14155238886',  
+                              body='fire detected at 30.236640,-97.821456',      
+                              to='whatsapp:+919344012834' 
+                          ) 
+ 
+GPIO.add_event_detect(channel, GPIO.FALLING, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+GPIO.add_event_detect(channel2, GPIO.FALLING, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel2, callback)
+# infinite loop
+while True:
+        time.sleep(1)
